2a6dfd24390369ed06530d0695cff9c215337201
Romeo Van Snick <romeovs@gmail.com>
2018-02-07 12:37:53 +0100

    Add ctrl bindings in command mode

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,9 @@ function fish_user_key_bindings
   bind -M insert \ca beginning-of-line
   bind -M insert \ce end-of-line
   bind -M insert \ck kill-line
+  bind \ca beginning-of-line
+  bind \ce end-of-line
+  bind \ck kill-line
   bind -M insert § "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
   bind -M insert \e\[A up-or-search
   bind -M insert \e\[B down-or-search
